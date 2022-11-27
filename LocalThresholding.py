a811e3f00b37223577bafaf92216d67e4f449986
yliwang <1220264221@qq.com>
2022-11-27 08:54:46 +0800

    Add files via upload

---
+++ LocalThresholding.py
@@ -0,0 +1,27 @@
+import numpy as np
+import PIL.Image as Image
+import sys
+import matplotlib.pyplot as plt
+
+# this function is for calculating a threshold using the two-means thresholding method, 
+# the method and reference implemention is presented in the lecture.
+# one can confidently borrow code from there.
+def TwoMeansThresholding(hist, prec):
+
+    return T
+
+
+# this function implements local thresholding, 
+# fn is an input image file.
+# gn is the name of the resulting binairzed image file, including path.
+# the function returns 1 on success, 0 if failed. 
+def LocalThresholding( fn, gn ):
+
+    return 1
+
+# this part is for testing
+if __name__=='__main__':
+    fn = ''
+    gn = './bin_nonuniform_illumination.bmp'
+    r = LocalThresholding( fn, gn)
+    
